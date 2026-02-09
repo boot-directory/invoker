6ad517de93881d1ae67bf7b5bcab19ce5a8a05d0
KovalevDima <onarainydayillwinanyway@gmail.com>
2026-02-09 03:23:27 +0300

    Use counter to calculate packets

--- libraries/invoker/invoker.hs
+++ libraries/invoker/invoker.hs
@@ -3,20 +3,29 @@ module Main where
 import System.IO (openBinaryFile, IOMode (..), Handle, hClose)
 import Control.Monad (forever)
 import Data.ByteString as BS (hGetSome)
+import Data.IORef (newIORef, modifyIORef, readIORef)
+import Data.Word (Word64)
 
 import Invoker (readOuterMessage, readHeader, mkBuffer, readFromBuffer, BufferArgs(..))
+import Control.Exception (bracket, onException)
 
 main :: IO ()
 main = do
+  counter <- newIORef (0 :: Word64)
+
   h <- openBinaryFile "./libraries/invoker/demos/8540916823.dem" ReadMode
   buf <- mkBuffer (fileBufferArgs h)
 
   header <- readFromBuffer buf readHeader
   print header
 
-  forever $ do
-    msg <- readFromBuffer buf readOuterMessage
-    print msg
+  onException
+    ( forever $ do
+      _msg <- readFromBuffer buf readOuterMessage
+      modifyIORef counter (+1)
+      pure ()
+    )
+    (print =<< readIORef counter)
 
 fileBufferArgs :: Handle -> BufferArgs
 fileBufferArgs h = MkBufferArgs
